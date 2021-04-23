de0588b30d40c7edcd12683dfad0b7aa42eaa365
Thato <pooet02@gmail.com>
2021-04-23 12:03:45 +0200

    port binding

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const dataSources = () => ({
 
 const server = new ApolloServer({ schema, dataSources })
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
